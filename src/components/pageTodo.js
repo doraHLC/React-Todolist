25bf1c2668fdba8a66d1d1aa6bf66ecbc8a4e52c
洧杰 廖 <gonsakon@gmail.com>
2022-07-04 15:57:33 +0800

    add todo router

---
+++ src/components/pageTodo.js
@@ -0,0 +1,10 @@
+function Todo() {
+  return (
+    <main>
+      <h2>todolist</h2>
+      <p>That feels like an existential question, don't you think?</p>
+    </main>
+  );
+}
+
+export default Todo;
